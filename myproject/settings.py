4dbba78d0c2fa36d17a5097daa1d1e7f4f7be409
Vibhanshu Jaiswal <98278084+vibhanshujais@users.noreply.github.com>
2025-04-24 21:24:59 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -140,8 +140,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'vibhanshujaiswal28@gmail.com'  # Replace with your email
-EMAIL_HOST_PASSWORD = 'eqjklgwbephvazpo'  # Replace with your app-specific password
+EMAIL_HOST_USER = 'your@gmail.com'  # Replace with your email
+EMAIL_HOST_PASSWORD = 'your-app-password'  # Replace with your app-specific password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
